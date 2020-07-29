5955025eff57bf8e5ecaf22322bffd458233ec4f
a <seichiclick081@gmail.com>
2020-07-30 05:28:30 +0900

    Discord ID 毎でユニーク化はできた。

--- EditCSV.py
+++ EditCSV.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import glob
+import math
 
 #複数ファイル読み込み。1つのデータフレームに。
-allFiles = glob.glob("*.csv")
+allFiles = glob.glob("Source/*.csv")
 frame = pd.DataFrame()
 list_ = []
 for file_ in allFiles:
     df = pd.read_csv(file_,index_col=None,header=0,encoding="utf-8")
+    #Discord ID でユニーク化
+    df = df.drop_duplicates(["Discord ID"])
     list_.append(df)
 df = pd.concat(list_)
+df = df["Discord ID"].value_counts()
+df.to_csv("Output/output.csv")
